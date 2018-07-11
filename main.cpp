839f0f8e3389001974a66d83f1ab9d060609f491
Konrad W <darnokusex@wp.pl>
2018-07-11 23:32:47 +0200

    initial comit with gitignore

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * RivieraDoorLock.cpp
+ *
+ * Created: 11.07.2018 23:24:28
+ * Author : Konrad
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
